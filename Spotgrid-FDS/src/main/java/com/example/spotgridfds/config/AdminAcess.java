95ddb3b9274a2eee3020c996c693c9661be0d366
Gustavo Lopes <80708475+GustavoLopes-dev@users.noreply.github.com>
2024-06-19 19:07:59 -0300

    Add files via upload

---
+++ Spotgrid-FDS/src/main/java/com/example/spotgridfds/config/AdminAcess.java
@@ -0,0 +1,11 @@
+package com.example.spotgridfds.config;
+
+public class AdminAcess {
+
+    private String adminPassword = "meAce$$@dmin24";
+    public AdminAcess() { }
+
+    public  String getAdminPass() {
+        return adminPassword;
+    }
+}
--- Spotgrid-FDS/src/main/java/com/example/spotgridfds/domain/Aplicativo.java
+++ Spotgrid-FDS/src/main/java/com/example/spotgridfds/domain/Aplicativo.java
@@ -1,4 +1,42 @@
 package com.example.spotgridfds.domain;
 
+import com.example.spotgridfds.config.AdminAcess;
+
 public class Aplicativo {
+    private AdminAcess adminAcess = new AdminAcess();
+    private static long codigoAppControle = 0;
+    private long codigoApp;
+    private String nomeApp;
+    private double custoAppMensal;
+
+    public Aplicativo (String nomeApp, double custoAppMensal) {
+        this.nomeApp        = nomeApp;
+        this.custoAppMensal = custoAppMensal;
+        this.codigoApp = codigoAppControle++;
+    }
+
+    public long getCodigoApp() {
+        return codigoApp;
+    }
+
+    public String getNomeApp() {
+        return nomeApp;
+    }
+
+    public double getCustoAppMensal() {
+        return custoAppMensal;
+    }
+
+    public boolean setNomeApp(String passAdmin, String novoNome) {
+        if (passAdmin != null && passAdmin.equals(getAdminKey())) {
+            this.nomeApp = novoNome;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getAdminKey() {
+        return adminAcess.getAdminPass();
+    }
 }
